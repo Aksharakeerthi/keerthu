712365bcedd768a52ba1c3bfdda76be0a59b1efe
Aksharakeerthi <akshara2223@gmail.com>
2017-07-06 18:15:40 +0530

    Create keerthu.java

---
+++ keerthu.java
@@ -0,0 +1,16 @@
+# keerthu
+import java.io.*;
+import java.util.*;
+public class keerthu{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int m;
+System.out.println("Enter the number");
+m=sc.nextInt();
+if(m%2==0){
+System.out.println("Number is even");
+}
+else
+System.out.println("Number is odd");
+}
+}
